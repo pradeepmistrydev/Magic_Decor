a65c1b8c442ce95c91454463d459e3f016ef5cfd
pradeepmistrydev <chauhanpradeep341@gmail.com>
2023-07-26 00:41:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+// owlCarousel slider
+
+$('.banner_carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 1,
+            nav: true
+        },
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+// owlCarousel slider
+
+
+// owlCarousel testimonial slider
+
+$('.testimonial_carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+// owlCarousel testimonial slider
+
+//transition
+
+$(function () {
+    $(".transition_content").twentytwenty();
+});
+
+//transition
+
+//nav active
+$(document).on('click', '.nav-item a', function (e) {
+    $(this).parent().addClass('active').siblings().removeClass('active');
+});
+//nav active
+
+//scroll btn 
+$('.topscroll').hide();
+$(window).scroll(function () {
+    $('.topscroll').show();
+});
+
+function scrollToTop() {
+    $(window).scrollTop(0);
+}
+//scroll btn 
